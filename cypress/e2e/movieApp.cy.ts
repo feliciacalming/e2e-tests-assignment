bcdfd24d6de4cff97375308d2e8fe54f6378f87f
Felicia Calming <felicia.calming@gmail.com>
2022-12-06 22:36:44 +0100

    added tests for reloading page, new search and made some small changes in existing tests

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -1,55 +1,110 @@
-import { IMovie, Movie } from "./../../src/ts/models/Movie";
-
-let mockMovies: IMovie[] = [
-  {
-    Title: "Léon",
-    imdbID: "1234",
-    Type: "movie",
-    Poster: "imageURL",
-    Year: "2022",
-  },
-];
-
 describe("movieApp", () => {
-  it("should be able to type", () => {
+  it("should show search form", () => {
+    cy.visit("http://localhost:1234");
+    cy.get("form").should("have.id", "searchForm");
+  });
+
+  it("should show button", () => {
+    cy.visit("http://localhost:1234");
+    cy.get("button").should("have.id", "search");
+  });
+
+  it("should be able to type in input", () => {
     cy.visit("http://localhost:1234");
     cy.get("input").type("star").should("have.value", "star");
   });
 
-  it("should", () => {
+  it("should get input value", () => {
+    let inputValue: string = "Léon";
+
     cy.visit("http://localhost:1234");
-    cy.get("input").type("star").should("have.value", "star");
+    cy.get("input").type(inputValue).should("have.value", inputValue);
   });
 
-  it("should get data from omdb", () => {
+  ////
+  it("should get data with 10 movies from omdb and present each movie in a div", () => {
     cy.visit("http://localhost:1234");
     cy.get("input").type("star").should("have.value", "star");
-    cy.get("button").click();
+    cy.get("form").submit();
+    // cy.get("button").click();
+
     cy.get("div:first > div").should("have.length", 10);
-    cy.get("div:first > div:first").contains(
-      "Star Wars: Episode IV - A New Hope"
+    cy.get("div div:first").contains("Star Wars: Episode IV - A New Hope");
+    cy.get("div div:nth-child(2)").contains(
+      "Star Wars: Episode V - The Empire Strikes Back"
     );
   });
 
   it("should not get data from omdb", () => {
     cy.visit("http://localhost:1234");
-    cy.get("input").type("s").should("have.value", "s");
+    cy.get("input").type(" ").should("have.value", " ");
     cy.get("button").click();
-    cy.get("div:first > div").should("have.length", 0);
+    cy.get("#movie-container").contains("Inga sökresultat att visa");
   });
 
-  it("should get mock data", () => {
-    let inputValue = "lord";
+  it("should replace existing movie result in div with new search results", () => {
+    let firstInputValue = "Evangelion";
+    let secondInputValue = "Sailor Moon";
+
     cy.visit("http://localhost:1234");
-    cy.intercept("GET", "https:omdbapi.com/apikey=db07c8df&s=*", mockMovies).as(
+    cy.get("input").type(firstInputValue).should("have.value", firstInputValue);
+    cy.get("button").click();
+
+    cy.get("div:first > div").should("have.length", 10);
+    cy.get("div div:first").contains("Evangelion");
+
+    cy.get("input")
+      .clear()
+      .type(secondInputValue)
+      .should("have.value", secondInputValue);
+    cy.get("button").click();
+
+    cy.get("div:first > div").should("have.length", 10);
+    cy.get("div div:first").contains("Sailor Moon");
+  });
+
+  it("should clear input text and erase movie-divs when reloading page", () => {
+    cy.visit("http://localhost:1234");
+    cy.get("input").type("Twin Peaks").should("have.value", "Twin Peaks");
+    cy.get("button").click();
+
+    cy.get("div > div").should("have.length", 10);
+    cy.get("div div:first").contains("Twin");
+
+    //after reloading page
+    cy.reload();
+    cy.get("input").should("have.value", "");
+    cy.get("div").should("have.length", 1);
+  });
+
+  ///// MOCKTESTER
+
+  it("should get and present mock data no matter what the input value is", () => {
+    let inputValue = "lord";
+    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "movies" }).as(
       "moviecall"
     );
+    cy.visit("http://localhost:1234");
     cy.get("input").type(inputValue).should("have.value", inputValue);
     cy.get("button").click();
 
     //
-    cy.wait("@moviecall").its("request.url").should("contain", inputValue);
+    cy.wait("@moviecall").its("request.url").should("contain", "lord");
+    cy.get("div > div:first").contains("Léon");
 
     // apikey=db07c8df&s=
+  });
+
+  it("should not get mock data and display error message", () => {
+    let inputValue = "Titanic";
+    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "emptyMovies" }).as(
+      "moviecall"
+    );
+    cy.visit("http://localhost:1234");
+    cy.get("input").type(inputValue).should("have.value", inputValue);
+    cy.get("button").click();
+
+    //
+    cy.get("#movie-container").contains("Inga sökresultat att visa");
   });
 });
