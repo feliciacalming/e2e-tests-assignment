66d38cee3d77dfc748f5c0cd58f020de8e6cc525
Felicia Calming <felicia.calming@gmail.com>
2022-12-11 21:25:56 +0100

    made minor changes to make css selectors more specific

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -3,20 +3,18 @@ describe("movieApp", () => {
     cy.visit("index.html");
   });
 
-  let inputValue: string = "star";
+  let inputValue: string = "star wars";
 
   it("should display 10 movies from search result, each in their own div with title and poster", () => {
     cy.get("input").type(inputValue).should("have.value", inputValue);
     cy.get("form").submit();
 
     cy.get("#movie-container > div").should("have.length", 10);
-    cy.get("#movie-container div:first h3").contains(
-      "Star Wars: Episode IV - A New Hope"
-    );
-    cy.get("div div:nth-child(2)").contains(
-      "Star Wars: Episode V - The Empire Strikes Back"
-    );
-    cy.get("img").should("have.attr", "src").should("include", "http");
+    cy.get("#movie-container div:first h3").contains("Star Wars");
+    cy.get("#movie-container div:nth-child(2) h3").contains("Star Wars");
+    cy.get("div.movie img")
+      .should("have.attr", "src")
+      .should("include", "http");
   });
 
   it("should display error message", () => {
@@ -26,33 +24,35 @@ describe("movieApp", () => {
   });
 
   it("should replace existing movie result in div with new search results", () => {
-    let firstInputValue = "Evangelion";
-    let secondInputValue = "Sailor Moon";
+    let firstInputValue: string = "Evangelion";
+    let secondInputValue: string = "Sailor Moon";
 
+    //first search
     cy.get("input").type(firstInputValue).should("have.value", firstInputValue);
     cy.get("button").click();
 
-    cy.get("div:first > div").should("have.length", 10);
-    cy.get("div div:first").contains("Evangelion");
+    cy.get("#movie-container > div").should("have.length", 10);
+    cy.get("#movie-container div:first h3").contains("Evangelion");
 
+    //clearing input + second search
     cy.get("input")
       .clear()
       .type(secondInputValue)
       .should("have.value", secondInputValue);
     cy.get("button").click();
 
-    cy.get("div:first > div").should("have.length", 10);
-    cy.get("div div:first").contains("Sailor Moon");
+    cy.get("#movie-container > div").should("have.length", 10);
+    cy.get("#movie-container div:first h3").contains("Sailor Moon");
   });
 
-  it("should clear input text and erase movie-divs when reloading page", () => {
+  it("should clear input text and erase movie-divs when refreshing page", () => {
     cy.get("input").type("Twin Peaks").should("have.value", "Twin Peaks");
     cy.get("button").click();
 
-    cy.get("div > div").should("have.length", 10);
-    cy.get("div div:first").contains("Twin");
+    cy.get("#movie-container > div").should("have.length", 10);
+    cy.get("#movie-container div:first h3").contains("Twin");
 
-    //after reloading page
+    //simulating that the user refreshes the page
     cy.reload();
     cy.get("input").should("have.value", "");
     cy.get("div").should("have.length", 1);
--- cypress/e2e/movieApp_testData.cy.ts
+++ cypress/e2e/movieApp_testData.cy.ts
@@ -3,7 +3,7 @@ describe("testing movieApp with test data", () => {
     cy.visit("index.html");
   });
 
-  let inputValue = "star";
+  let inputValue: string = "star";
 
   it("should show search form", () => {
     cy.get("form").should("have.id", "searchForm");
@@ -14,15 +14,14 @@ describe("testing movieApp with test data", () => {
   });
 
   it("should be able to type in input", () => {
-    cy.get("input").type("star").should("have.value", "star");
-  });
-
-  it("should get input value", () => {
-    // let inputValue: string = "Léon";
     cy.get("input").type(inputValue).should("have.value", inputValue);
   });
 
-  it("should search for input value but show test data", () => {
+  it("should get input value", () => {
+    cy.get("input").type(inputValue).should("have.value", inputValue);
+  });
+
+  it("should search for input value but return test data", () => {
     cy.intercept("GET", "http://omdbapi.com/*", { fixture: "movies" }).as(
       "moviecall"
     );
@@ -32,19 +31,31 @@ describe("testing movieApp with test data", () => {
 
     cy.wait("@moviecall").its("request.url").should("contain", "star");
     cy.get("div.movie:first h3").contains("Léon");
+    cy.get("div.movie:nth-child(2) h3").contains("Twin Peaks");
+    cy.get("div.movie:nth-child(3) h3").contains("Magnolia");
     cy.get("div.movie:first img")
       .should("have.attr", "src")
       .should("include", "http");
+  });
 
-    cy.get("div.movie:nth-child(2) h3").contains("Twin Peaks");
-    cy.get("div.movie:nth-child(3) h3").contains("Magnolia");
+  it("should display error message", () => {
+    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "emptyMovies" }).as(
+      "moviecall"
+    );
+
+    cy.get("input").type(inputValue).should("have.value", inputValue);
+    cy.get("button").click();
+
+    cy.wait("@moviecall").its("request.url").should("contain", inputValue);
+    cy.get("div").should("have.length", 1);
+    cy.get("p").contains("sökresultat");
   });
 
   it("should replace existing movie result in div with new search results", () => {
-    //first search
     let firstInputValue: string = "Evangelion";
     let secondInputValue: string = "Sailor";
 
+    //first search
     cy.intercept("GET", "http://omdbapi.com/*", {
       fixture: "movies",
     }).as("moviecall");
@@ -54,10 +65,10 @@ describe("testing movieApp with test data", () => {
 
     cy.wait("@moviecall").its("request.url").should("contain", firstInputValue);
 
-    cy.get("div:first > div").should("have.length", 3);
+    cy.get("#movie-container > div").should("have.length", 3);
     cy.get("div.movie:first h3").contains("Léon");
 
-    //second search
+    //clearing input + second search with different test data
     cy.intercept("GET", "http://omdbapi.com/*", { fixture: "otherMovies" }).as(
       "secondMoviecall"
     );
@@ -72,7 +83,7 @@ describe("testing movieApp with test data", () => {
       .its("request.url")
       .should("contain", secondInputValue);
 
-    cy.get("div:first > div").should("have.length", 3);
+    cy.get("#movie-container > div").should("have.length", 3);
     cy.get("div.movie:first h3").contains("Lost In Translation");
   });
 
@@ -80,26 +91,12 @@ describe("testing movieApp with test data", () => {
     cy.get("input").type("Twin Peaks").should("have.value", "Twin Peaks");
     cy.get("button").click();
 
-    cy.get("div > div").should("have.length", 10);
-    cy.get("div div:first").contains("Twin");
+    cy.get("#movie-container > div").should("have.length", 10);
+    cy.get("#movie-container div:first h3").contains("Twin");
 
     //after reloading page
     cy.reload();
     cy.get("input").should("have.value", "");
     cy.get("div").should("have.length", 1);
-  });
-
-  //if movies.length = 0:
-  it("should display error message", () => {
-    cy.intercept("GET", "http://omdbapi.com/*", { fixture: "emptyMovies" }).as(
-      "moviecall"
-    );
-
-    cy.get("input").type(inputValue).should("have.value", inputValue);
-    cy.get("button").click();
-
-    cy.wait("@moviecall").its("request.url").should("contain", inputValue);
-    cy.get("div").should("have.length", 1);
-    cy.get("p").contains("sökresultat");
   });
 });
